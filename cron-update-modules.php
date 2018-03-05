a8cbb8138ce0f4009f7159fd0eb22f54e7b55191
Yudi Kondo <yudi@kreativos.com.br>
2018-03-05 15:40:51 -0300

    Fixed some bugs

--- cron-update-modules.php
+++ cron-update-modules.php
@@ -11,7 +11,7 @@ class ISC_UPGRADE_MODULES {
     /**
      * Scan all Checkout Modules
      */
-    public function scan_modules($dir)
+    public function scan_modules($dir, $dir_remote)
     {
         $folders = scandir($dir);
         foreach ($folders as $folder) {
@@ -20,24 +20,24 @@ class ISC_UPGRADE_MODULES {
                 if($this->check_exist_settings($dir, $folder))
                 {
                     $json_local  = $this->read_json_file($dir, $folder);
-                    $json_remote = $this->read_json_file($this->_remote_server, $this->_remote_checkout_modules.$folder);
+                    $json_remote = $this->read_json_file($this->_remote_server, $dir_remote.$folder);
                     if($json_local['version'] === $json_remote['version'])
-                        return false;
+                        $this->save_update_log("Data:".date('d/m/Y')." Módulo -> {$dir}{$folder} - Está na versão mais recente -> {$json_remote['version']}");
                     else 
                     {
-                        if($this->get_http_response_file($this->_remote_server, $this->_remote_checkout_modules.$folder) === '200')
+                        if($this->get_http_response_file($this->_remote_server, $dir_remote.$folder) === '200')
                         {
-                            if($this->download_new_module($this->_remote_server, $this->_remote_checkout_modules, $folder, 'module.zip', $dir) == TRUE)
+                            if($this->download_new_module($this->_remote_server, $dir_remote, $folder, 'module.zip', $dir) == TRUE)
                             {
                                 if($this->unzip_module($dir, $folder) == TRUE)
                                 {
-                                    $this->save_update_log('Ok');
+                                    $this->save_update_log("Data:".date('d/m/Y')." Módulo -> {$dir}{$folder} - Atualizado para a versão -> {$json_remote['version']} com Sucesso!");
                                     $this->delete_zip_file($dir, $folder);
                                 }
                             }
                         }
                         else 
-                            $this->save_update_log('File not found.');
+                            $this->save_update_log("Data:".date('d/m/Y')." Não foi encontrado o arquivo -> module.zip no servidor remoto.");
                     }
                 }
             }
@@ -107,11 +107,34 @@ class ISC_UPGRADE_MODULES {
         return unlink($dir.'/'.$folder.'/'.$file);
     }
 
+    public function create_table_logs()
+	{
+		$query = $GLOBALS['ISC_CLASS_DB']->Query("SHOW TABLES LIKE 'kreativos_cron_update_modules'");
+		$count = $GLOBALS['ISC_CLASS_DB']->CountResult($query);
+		if($count == 0)
+		{
+			$create_table = "CREATE TABLE kreativos_cron_update_modules 
+            (
+				id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+				log TEXT NOT NULL
+            ) 
+            DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci
+            ";
+			$query = $GLOBALS['ISC_CLASS_DB']->Query($create_table);
+			if($query)
+			{
+				return true;
+			}
+		}
+	}
+
     public function save_update_log($log)
     {
-
+        $this->create_table_logs();
+        $query = $GLOBALS['ISC_CLASS_DB']->Query("INSERT INTO kreativos_cron_update_modules (log) VALUES ('{$log}')");
     }
 }
 
 $modules = new ISC_UPGRADE_MODULES();
-$modules->scan_modules($modules->_checkout_modules);
+$modules->scan_modules($modules->_checkout_modules, $modules->_remote_checkout_modules);
+$modules->scan_modules($modules->_shipping_modules, $modules->_remote_shipping_modules);
